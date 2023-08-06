dc9784cb3ec3ffd5c805d1663d1a4f80e11a4519
Shodai Fukui <cmt14111@ict.nitech.ac.jp>
2023-08-06 19:08:11 +0900

    make function resetBoard

--- rule.py
+++ rule.py
@@ -13,3 +13,12 @@ class Person:
 
 board = [[Color.empty for _ in range(WIDTH)] for _ in range(HEIGHT)]
 change = [[-1 for _ in range(2)] for _ in range(21)]
+
+def resetBoard():
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            board[i][j] = Color.empty
+            if ((i == HEIGHT//2 and j == WIDTH//2) or (i == HEIGHT//2 - 1 and j == WIDTH//2 - 1)):
+                board[i][j] = Color.black
+            elif ((i == HEIGHT//2 - 1 and j == WIDTH//2) or (i == HEIGHT//2 and j == WIDTH//2 - 1)):
+                board[i][j] = Color.white
