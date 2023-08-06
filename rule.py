b042dca0a452b5e6427d41b61a1eb00998a6e784
Shodai Fukui <cmt14111@ict.nitech.ac.jp>
2023-08-06 19:17:11 +0900

    fix bug

--- rule.py
+++ rule.py
@@ -12,7 +12,7 @@ class Person:
         self.color = color
 
 board = [[Color.empty for _ in range(WIDTH)] for _ in range(HEIGHT)]
-change = [[-1 for _ in range(2)] for _ in range(21)]
+
 
 def resetBoard():
     for i in range(HEIGHT):
