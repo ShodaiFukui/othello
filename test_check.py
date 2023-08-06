c529a421a875e0482cc34475d19c1bcf2a7ab188
Shodai Fukui <cmt14111@ict.nitech.ac.jp>
2023-08-06 23:18:24 +0900

    make test_check6

--- test_check.py
+++ test_check.py
@@ -6,7 +6,7 @@ import rule
 # 1
 # 2
 # 3         ○ ●
-# 4       ○ ●
+# 4       ○ ● ●
 # 5       ● ○
 # 6
 # 7
@@ -23,8 +23,8 @@ class TestMyTest(unittest.TestCase):
         self.assertEqual(rule.Check.NG, result)
         
     def test_check3(self):
-        result = rule.check(5, 2, rule.Color.white)
-        self.assertEqual(rule.Check.OK, result)
+        result = rule.check(4, 3, rule.Color.black)
+        self.assertEqual(rule.Check.NG, result)
     
     def test_check4(self):
         result = rule.check(4, 3, rule.Color.white)
@@ -34,8 +34,13 @@ class TestMyTest(unittest.TestCase):
         result = rule.check(3, 6, rule.Color.white)
         self.assertEqual(rule.Check.NG, result)
         
+    def test_check6(self):
+        result = rule.check(3, 6, rule.Color.white)
+        self.assertEqual(rule.Check.NG, result)
+        
 if __name__ == "__main__":
     rule.resetBoard()
     rule.board[2][4] = rule.Color.black
     rule.board[2][5] = rule.Color.white
+    rule.board[3][5] = rule.Color.white
     unittest.main()
