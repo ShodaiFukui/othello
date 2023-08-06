13e2e5126c84ebdea0b61b7f19291975697d5cca
Shodai Fukui <cmt14111@ict.nitech.ac.jp>
2023-08-06 22:46:57 +0900

    make test_check1

---
+++ test_check.py
@@ -0,0 +1,25 @@
+import unittest
+import rule
+
+#以下の盤面でのテストを行う
+#   1 2 3 4 5 6 7 8
+# 1
+# 2
+# 3         ○ ●
+# 4       ○ ●
+# 5       ● ○
+# 6
+# 7
+# 8
+
+class TestMyTest(unittest.TestCase):
+    
+    def test_check1(self):
+        result = rule.check(1, 1, rule.Color.black)
+        self.assertEqual(rule.Check.NG, result)
+        
+if __name__ == "__main__":
+    rule.resetBoard()
+    rule.board[4][2] = rule.Color.black
+    rule.board[5][2] = rule.Color.white
+    unittest.main()
