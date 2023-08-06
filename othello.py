e8424a811196bd7d5023819b76c38ef7dbfd5242
Shodai Fukui <cmt14111@ict.nitech.ac.jp>
2023-08-06 19:24:53 +0900

    fix main

--- othello.py
+++ othello.py
@@ -5,6 +5,22 @@ import printResult
 def main():
     rule.resetBoard()
     rule.printBoard()
+    
+    while True:
+        mode = input("モード選択. 0..検証用/1..プレイ: ")
+        choice = input("あなたの碁の色を入力して下さい. 'black' or 'white': ")
+        if choice == "black":
+            player = rule.Person_of(rule.Color.black)
+            npc = rule.Person_of(rule.Color.white)
+        elif choice == "white":
+            player = rule.Person_of(rule.Color.white)
+            npc = rule.Person_of(rule.Color.black)
+        else:
+            print("'black'または'white'を入力して下さい．")
+            continue
+        break
+    
+    gameFlow.game(player.color, npc.color, mode)
 
 if __name__ == "__main__":
     main()
